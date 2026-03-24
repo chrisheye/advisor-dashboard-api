17a8ba1eaf1871c31130b497281e9cc5cfcfb608
chrisheye <chris@whealthcareplan.com>
2026-03-24 11:17:09 -0700

    Update advisor_main.py

--- advisor_main.py
+++ advisor_main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from sqlalchemy import create_engine, text, MetaData, Table, Column, String
+from sqlalchemy import create_engine, text, MetaData, Table, Column, String, JSON
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
