a73644a3648bbb4315ffd392b0b51ec9f527b8e2
chrisheye <chris@whealthcareplan.com>
2026-08-25 08:49:46 -0700

    Update advisor_main.py

--- advisor_main.py
+++ advisor_main.py
@@ -64,7 +64,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://app.whealthchat.ai"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
